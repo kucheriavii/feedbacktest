2c9ad3cbbb7e70236b5fb9418f423a5afbf28e5e
kucheriavii <kucheriavii@gmail.com>
2022-08-11 19:46:28 +0300

    2. Adding Styles To A Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,14 @@
+import Header from "./components/Header";
+
 function App() {
 
-    const title = 'Blog post'
-    const body = 'This is my blog post'
-
-    const comments = [
-        {id:1, text: "comment one"},
-        {id:2, text: "comment two"},
-        {id:3, text: "comment three"}
-    ]
-
-    const loading = false
-    const showComents = true
-
-    if (loading) return <h1>Loading...</h1>
-
-    const commentBlock = (
-        <div className='comments'>
-            {comments.map((comment, index) => {
-                return <li key={comment.id}>{comment.text}</li>
-            })}    
-        </div>  
-        )
-    
-
     return (
-    <div className="container">
-        <h1>{title}</h1>
-        <p>{body}</p>
-
-        {showComents && commentBlock}
-
-        <p>{body.toUpperCase()}</p>
-
-        
-    </div>
+    <>
+        <Header text="Hello world"/>
+        <div className="container">
+            <h1>My App</h1>
+        </div>
+    </>
     )
 }
 
