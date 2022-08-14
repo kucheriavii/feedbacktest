5ffba643e48b2fd2bece55eeda487a04a5b0a66e
kucheriavii <kucheriavii@gmail.com>
2022-08-14 19:49:14 +0300

    1. Creating Routes (React Router 6)

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,10 @@ import FeedbackData from './data/FeedbackData';
 import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
 import {v4 as uuidv4} from 'uuid';
+import AboutPage from './pages/AboutPage';
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
+
+
 
 function App() {
     const [feedback, setFeedback] = useState(FeedbackData)
@@ -21,14 +25,22 @@ function App() {
         setFeedback([...feedback, newFeedback])
     }
     return (
-    <>
+    <Router>
         <Header text="Hello world"/>
         <div className="container">
-            <FeedbackForm handleAdd = {addFeedback}/>
-            <FeedbackStats feedback={feedback}/>
-            <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
+            <Routes>
+                <Route exact path= "/" element={
+                    <>
+                        <FeedbackForm handleAdd = {addFeedback}/>
+                        <FeedbackStats feedback={feedback}/>
+                        <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
+                    </>
+                }>
+                </Route>
+                <Route path="/about" element={<AboutPage />} />
+            </Routes>
         </div>
-    </>
+    </Router>
     )
 }
 
